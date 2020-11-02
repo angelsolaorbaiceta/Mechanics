076af4cf16a994c50c34bc00d33f48ec08d77bec
angelsolaorbaiceta <angelsolaorbaiceta@gmail.com>
2020-11-02 07:59:08 +0100

    Document the structure solution class

--- structures/solution/structure.py
+++ structures/solution/structure.py
@@ -7,6 +7,12 @@ from .node import StrNodeSolution
 
 
 class StructureSolution:
+    """
+    Truss structure solution model.
+
+    The solution model groups the list of solution nodes and the
+    list of solution bars.
+    """
 
     def __init__(
             self,
@@ -17,6 +23,17 @@ class StructureSolution:
         self.bars = bars
 
     def bounds_rect(self, margin: float, scale=1):
+        """
+        Computes the rectangle that contains all the structure
+        displaced nodes and including a given margin.
+
+        This rectangular bounds can be used to compute the solution
+        diagram's size.
+
+        :param margin: extra space added to the sides
+        :param scale: scale used for the node displacements
+        :return: rectangular bounds
+        """
         d_pos = [
             node.displaced_pos_scaled(scale)
             for node in self.nodes
@@ -24,6 +41,19 @@ class StructureSolution:
         return make_rect_containing_with_margin(d_pos, margin)
 
     def reaction_for_node(self, node: StrNodeSolution):
+        """
+        Computes the external reaction force for a given node.
+
+        To compute the reaction, it takes into account the external
+        loads and the internal forces from the bars connected to
+        the node.
+
+        A node that isn't externally constrained has no reaction
+        force.
+
+        :param node: node to compute the reaction force
+        :return: reaction force on the node
+        """
         if not node.is_constrained:
             return Vector(0, 0)
 
